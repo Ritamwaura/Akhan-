1bab417569e26fc1d3f2ce1e08e496d171852753
Ritamwaura <mwaurarita2019@gmail.com>
2019-07-15 12:23:19 +0300

    add change

--- script0.js
+++ script0.js
@@ -1,67 +1,67 @@
-var cen, year, month, date, day, gender;
-var maleNames["kwasi", "kwadwo", "kwabena", "kwaku", "yaw", "kofi", "kwame"];
-var femaleNames["akosua", "adwoa", "abenaa", "akwa", "yaa", "afua", "ama"];
 
+var maleNames = ["kwasi", "kwadwo", "kwabena", "kwaku", "yaw", "kofi", "kwame"];
+var femaleNames = ["akosua", "adwoa", "abenaa", "akwa", "yaa", "afua", "ama"];
+var cen, year, month, date;
 //ask for input
 
 function input(){
-  cen = document.getElementById('century').value;
-  year =document.getElementByid('year').value;
-  month =document.getElementByid('month').value;
-  date =document.getElementById('date').value;
-  day =document.getElementById('day').value;
+  cen = parseInt(document.getElementById('century').value);
+  year =parseInt(document.getElementById('year').value);
+  month =parseInt(document.getElementById('month').value);
+  date =parseInt(document.getElementById('date').value);
 }
 
 
 function calc(){
   input();
-  day = ( ( (cen/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+  day = (((cen/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
     return day = (Math.floor(day));
 }
-
-var radio = document.getElementsByName("rad");
-
-if (rad[0].checked = true) {
-  gender = "male";
-}else if(rad[1].checked == true){
-  gender == "female";
-}else{
-  console.log("pass");
-}
+  var result=calc();
 
 function check(){
   calc();
+  var radio = document.getElementsByName("rad");
+
+  if (radio[0].checked = true) {
+    gender = "male";
+  }else if(radio[1].checked == true){
+    gender = "female";
+  }else{
+    console.log("pass");
+  }
+
   if (gender == "male") {
-    if (day == 0) {
-      alert("You were born on a sunday" + male Names[0]);
+    if (day == 0){
+      alert("You were born on a sunday" + maleNames[0]);
     }else if(day == 1){
-      alert("You were born on a monday" + male Names[1]);
+      alert("You were born on a monday" + maleNames[1]);
     }else if(day == 2){
-      alert("You were born on a tuesday" + male Names[2]);
+      alert("You were born on a tuesday" + maleNames[2]);
     }else if(day == 3){
-      alert("You were born on a wednesday"+ male Names[3]);
+      alert("You were born on a wednesday"+ maleNames[3]);
     }else if(day == 4){
-      alert("You were born on a thursday" + male Names[4]);
+      alert("You were born on a thursday" + maleNames[4]);
     }else if(day == 5){
-      alert("You were born on a friday" + male Names[5]);
+      alert("You were born on a friday" + maleNames[5]);
     }else if(day == 6){
-      alert("You were born on a saturday" + male Names[6]);
+      alert("You were born on a saturday" + maleNames[6]);
     }
   }else if(gender == "female"){
     if (day == 0) {
-      alert("You were born on a sunday" + female Names[0]);
+      alert("You were born on a sunday" + femaleNames[0]);
     }else if(day == 1){
-      alert("You were born on a monday" + female Names[1]);
+      alert("You were born on a monday" + femaleNames[1]);
     }else if(day == 2){
-      alert("You were born on a tuesday" + female Names[2]);
+      alert("You were born on a tuesday" + femaleNames[2]);
     }else if(day == 3){
-      alert("You were born on a wednesday"+ female Names[3]);
+      alert("You were born on a wednesday"+ femaleNames[3]);
     }else if(day == 4){
-      alert("You were born on a thursday" + female Names[4]);
+      alert("You were born on a thursday" + femaleNames[4]);
     }else if(day == 5){
-      alert("You were born on a friday" + female Names[5]);
+      alert("You were born on a friday" + femaleNames[5]);
     }else if(day == 6){
-      alert("You were born on a saturday" + female Names[6]);
+      alert("You were born on a saturday" + femaleNames[6]);
     }
   }
 
